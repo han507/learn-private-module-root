59033eb827e9b95d7e3620f113025e4e01716ac2
한병무(ByungMoo Han)/Service Eng.팀/SKP <moomoo@sk.com>
2021-01-11 17:11:22 +0900

    modify

--- main.tf
+++ main.tf
@@ -3,9 +3,13 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/SKPlanet/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
 }
+# terraform-<PROVIDER>-<NAME>
+# https://github.com/han507/terraform-aws-s3-webapp
+# app.terraform.io/<ORGANIZATION-NAME>/terraform/<NAME>/<PROVIDER>
+# app.terraform.io/SKPlanet/terraform/s3-webapp/aws
